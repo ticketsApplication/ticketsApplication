eef0476a1bb288e8d9356305d0dc99cf99cf0092
Andrii Horbenko <andrewhorbenko@gmail.com>
2022-11-14 13:29:03 +0200

    first commit to trips package

--- src/main/java/org/stepProjectBooking/Main.java
+++ src/main/java/org/stepProjectBooking/Main.java
@@ -1,7 +1,18 @@
 package org.stepProjectBooking;
 
+import org.stepProjectBooking.ticketsApplication.trips.Departures;
+import org.stepProjectBooking.ticketsApplication.trips.Trip;
+import org.stepProjectBooking.ticketsApplication.trips.TripCreator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        TripCreator tripCreator = new TripCreator();
+        Trip trip;
+
+        for (int i = 0; i < 10; i++) {
+            trip =tripCreator.createTrip();
+            System.out.println(trip.prettyFormat());
+        }
     }
 }
