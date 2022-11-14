9f05eb27e6f3905da3b065812ba6bd4c4075995f
BukanNadya <118114796+BukanNadya@users.noreply.github.com>
2022-11-14 15:24:42 +0200

    Trip_DAO,_User

--- src/main/java/org/stepProjectBooking/ticketsApplication/DAO/tripDAO/CollectionTripDao.java
+++ src/main/java/org/stepProjectBooking/ticketsApplication/DAO/tripDAO/CollectionTripDao.java
@@ -1,4 +1,24 @@
-package org.stepProjectBooking.tripDAO;
+package org.stepProjectBooking.ticketsApplication.DAO.tripDAO;
 
-public class CollectionTripDao {
+import org.stepProjectBooking.ticketsApplication.trips.Trip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionTripDao implements TripDao {
+    private final List<Trip> trips;
+
+    public CollectionTripDao(List<Trip> trip) {
+        this.trips = new ArrayList<>();
+    }
+
+    @Override
+    public ArrayList<Trip> getTrips() {
+        return (ArrayList<Trip>) trips;
+    }
+
+    @Override
+    public void addTrip(Trip trip) {
+        trips.add(trip);
+    }
 }
