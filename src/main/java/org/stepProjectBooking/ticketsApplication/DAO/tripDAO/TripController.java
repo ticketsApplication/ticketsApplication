98541ba29569053129da81fdc0c4ce31aa2bf05d
BukanNadya <118114796+BukanNadya@users.noreply.github.com>
2022-11-14 21:45:29 +0200

    Trip_DAO,_User (#3)

--- src/main/java/org/stepProjectBooking/ticketsApplication/DAO/tripDAO/TripController.java
+++ src/main/java/org/stepProjectBooking/ticketsApplication/DAO/tripDAO/TripController.java
@@ -1,4 +1,23 @@
-package org.stepProjectBooking.tripDAO;
+package org.stepProjectBooking.ticketsApplication.DAO.tripDAO;
+
+
+import org.stepProjectBooking.ticketsApplication.trips.Trip;
+
+import java.util.ArrayList;
 
 public class TripController {
+
+    private final TripService tripService;
+
+    public TripController() {
+        this.tripService = new TripService();
+    }
+
+    public ArrayList<Trip> getTrips() {
+        return tripService.getTrip();
+    }
+
+    public void addTrip(Trip trip) {
+        tripService.addTrip(trip);
+    }
 }
--- src/main/java/org/stepProjectBooking/ticketsApplication/user/Passenger.java
+++ src/main/java/org/stepProjectBooking/ticketsApplication/user/Passenger.java
@@ -1,4 +1,27 @@
 package org.stepProjectBooking.ticketsApplication.user;
 
-public class Passenger {
+public class Passenger extends User {
+
+    private String tripId;
+    int bookId;
+
+    public Passenger(String name, String surname) {
+        super(name, surname);
+    }
+
+    public Passenger(String name, String surname, String tripId, int bookId) {
+        super(name, surname);
+        this.tripId = tripId;
+        this.bookId = bookId;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "name='" + getName() + '\'' +
+                ", surname='" + getSurname() + '\'' +
+                "tripId='" + tripId + '\'' +
+                ", bookId=" + bookId +
+                '}';
+    }
 }
