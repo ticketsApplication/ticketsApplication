63991756a96add6355bcc9f068c7a76f60f2d0b1
BukanNadya <118114796+BukanNadya@users.noreply.github.com>
2022-11-17 10:10:23 +0200

    TripDaoImprovements (#5)

--- src/main/java/org/stepProjectBooking/Main.java
+++ src/main/java/org/stepProjectBooking/Main.java
@@ -1,6 +1,10 @@
 package org.stepProjectBooking;
 
+import org.stepProjectBooking.ticketsApplication.DAO.tripDAO.TripController;
 import org.stepProjectBooking.ticketsApplication.trips.Departures;
+import org.stepProjectBooking.ticketsApplication.trips.Trip;
+import org.stepProjectBooking.ticketsApplication.trips.TripCreator;
+import org.stepProjectBooking.ticketsApplication.user.User;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +16,10 @@ public class Main {
             trip =tripCreator.createTrip();
             System.out.println(trip.prettyFormat());
         }
+
+        TripController tripController = new TripController();
+
+
 
     }
 }
